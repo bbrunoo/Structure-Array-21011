040bc57b9c89cfe581d2221605ec1c9ab488c47f
bruno.sanntos <brunoedsanntos@gmail.com>
2021-09-20 15:22:56 -0300

    commit inicial

---
+++ ArrayCollection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace ArrayCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alunosAMS =  new string[5]{
+                "Dani",
+                "Maju",
+                "Alcino",
+                "Bruno",
+                "Talita",
+            };
+
+         
+            WriteLine(alunosAMS[2]);
+        
+            alunosAMS[2] = "Jeferson"; 
+
+            WriteLine($"Novo valor no index 2: {alunosAMS[2]}");
+
+            WriteLine("-----------------------------------------------");
+   
+
+            foreach (string aluno in alunosAMS)
+            {
+                WriteLine(aluno);
+            }
+
+            WriteLine("-----------------------------------------------");
+
+            for(int i = 0; i < alunosAMS.Lenght; i++) 
+            {
+                WriteLine(alunosAMS[i]);
+            } 
+        }
+    }
+}
